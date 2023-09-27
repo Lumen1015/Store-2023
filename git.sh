42cc828642142b09c427b030552ee5d031b954d5
Lumen1015 <2271039280szr@sina.com>
2023-09-27 10:12:40 +0800

    autopush

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 git config --global user.name 'Lumen1015'
-git config --global user.email '2271039280@sina.com'
+git config --global user.email '2271039280szr@sina.com'
 git config --global http.sslverify false
 
 git add .
-git commit -m $1
+git commit -m autopush
 git push
